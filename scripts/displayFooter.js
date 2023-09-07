296c7d8a0a8917b9219de0721510fb158d53b0e2
Keviinbrand <kevinbrand2004@gmail.com>
2023-09-07 05:35:41 -0500

    función mostrar tareas según categoria

--- scripts/displayFooter.js
+++ scripts/displayFooter.js
@@ -1,24 +1,46 @@
-
 function displayFooter() {
   let page = `
-
       <footer class="footer">
-
         <span class="todo-count"><strong></strong> pendiente(s)</span>
-
         <ul class="filters">
           <li>
-            <a class="selected filtro" href="#/">Todos</a>
+            <a class="selected filtro" href="#/" onclick="showAll()">Todos</a>
           </li>
           <li>
-            <a class="filtro" href="#/active">Pendientes</a>
+            <a class="filtro" href="#/active" onclick="showPendientes()">Pendientes</a>
           </li>
           <li>
-            <a class="filtro" href="#/completed">Completados</a>
+            <a class="filtro" href="#/completed" onclick="showCompletadas()">Completadas</a>
           </li>
         </ul>
         <button class="clear-completed">Borrar completados</button>
       </footer>
-    `
-  document.querySelector('.footer').innerHTML = page
+    `;
+  document.querySelector('.footer').innerHTML = page;
+}
+
+// Función para mostrar tareas pendientes
+function showPendientes() {
+  const pendientes = document.querySelectorAll('.item');
+  pendientes.forEach((element) => {
+    const checkbox = element.querySelector('.toggle');
+    if (checkbox.checked) {
+      element.style.display = 'none';
+    } else {
+      element.style.display = 'block';
+    }
+  });
+}
+
+// Función para mostrar tareas completadas
+function showCompletadas() {
+  const completadas = document.querySelectorAll('.item');
+  completadas.forEach((element) => {
+    const checkbox = element.querySelector('.toggle');
+    if (!checkbox.checked) {
+      element.style.display = 'none';
+    } else {
+      element.style.display = 'block';
+    }
+  });
 }
